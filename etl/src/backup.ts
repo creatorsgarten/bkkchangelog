236c95c1283aff7e767a39f4506d6be696bcd35b
Thai Pangsakulyanont <dtinth@spacet.me>
2023-03-01 13:36:16 +0000

    refactor

--- etl/src/backup.ts
+++ etl/src/backup.ts
@@ -3,24 +3,20 @@ import { ConnectFn, getTicketSnapshotsCollection, withDb } from './_db'
 
 async function main(connect: ConnectFn) {
   const client = await connect()
-  try {
-    const collection = getTicketSnapshotsCollection(client)
-    mkdirSync('.data', { recursive: true })
-    const stream = createWriteStream('.data/backup.ndjson')
-    let lastPrinted = 0
-    let count = 0
-    for await (const doc of collection.find()) {
-      stream.write(JSON.stringify(doc.data) + '\n')
-      count++
-      if (Date.now() - lastPrinted > 5000) {
-        console.log(`[${new Date().toISOString()}] ${count} records downloaded`)
-        lastPrinted = Date.now()
-      }
+  const collection = getTicketSnapshotsCollection(client)
+  mkdirSync('.data', { recursive: true })
+  const stream = createWriteStream('.data/backup.ndjson')
+  let lastPrinted = 0
+  let count = 0
+  for await (const doc of collection.find()) {
+    stream.write(JSON.stringify(doc.data) + '\n')
+    count++
+    if (Date.now() - lastPrinted > 5000) {
+      console.log(`[${new Date().toISOString()}] ${count} records downloaded`)
+      lastPrinted = Date.now()
     }
-    stream.end()
-  } finally {
-    await client.close()
   }
+  stream.end()
 }
 
 withDb(main)
--- etl/src/collect.ts
+++ etl/src/collect.ts
@@ -3,27 +3,17 @@ import { globbySync } from 'globby'
 import { z } from 'zod'
 import { AnyBulkWriteOperation, MongoClient } from 'mongodb'
 import { Env } from 'lazy-strict-env'
+import { ConnectFn, getTicketSnapshotsCollection, withDb } from './_db'
 
 const EntryInfo = z.object({
   ticket_id: z.coerce.string().min(1),
   last_activity: z.coerce.string(),
 })
 
-const env = Env(
-  z.object({
-    MONGODB_URI: z.string().url(),
-  }),
-)
-
-interface TicketSnapshot {
-  data: any
-  updated: Date
-}
-
-async function main() {
+async function main(connect: ConnectFn) {
   const files = globbySync('.data/fetched/*.json')
-  const client = await MongoClient.connect(env.MONGODB_URI)
-  const collection = client.db().collection<TicketSnapshot>('ticket_snapshots')
+  const client = await connect()
+  const collection = getTicketSnapshotsCollection(client)
 
   // Ensure that a unique index exists for (data.ticket_id, updated)
   await collection.createIndex(
@@ -33,49 +23,45 @@ async function main() {
   await collection.createIndex({ updated: -1 })
   await collection.createIndex({ state: 1 })
 
-  try {
-    let nextWarningId = 1
-    let totalProcessed = 0
-    let totalAdded = 0
-    for (const [fileIndex, file] of files.entries()) {
-      console.log(`[${fileIndex + 1}/${files.length}] Processing ${file}...`)
-      try {
-        const fileData = JSON.parse(readFileSync(file, 'utf-8'))
-        const operations = fileData.results.flatMap((data: any) => {
-          if (!data.ticket_id) {
-            console.log(
-              `[Warning #${nextWarningId++}] Missing ticket_id`,
-              JSON.stringify(data),
-            )
-            return []
-          }
-          const info = EntryInfo.parse(data)
-          const updated = new Date(Date.parse(info.last_activity))
-          updated.setMilliseconds(0)
-          const operation: AnyBulkWriteOperation<TicketSnapshot> = {
-            updateOne: {
-              filter: { 'data.ticket_id': info.ticket_id, updated },
-              update: { $setOnInsert: { data, updated } },
-              upsert: true,
-            },
-          }
-          return [operation]
-        })
-        const writeResult = await collection.bulkWrite(operations)
-        const nProcessed = operations.length
-        const nAdded = writeResult.getUpsertedIds().length
-        totalProcessed += nProcessed
-        totalAdded += nAdded
-        console.log(
-          `Processed ${file} (${totalProcessed} (+${nProcessed}) processed, ${totalAdded} (+${nAdded}) added)`,
-        )
-      } catch (error) {
-        console.error(`Unable to process ${file}`, error)
-      }
+  let nextWarningId = 1
+  let totalProcessed = 0
+  let totalAdded = 0
+  for (const [fileIndex, file] of files.entries()) {
+    console.log(`[${fileIndex + 1}/${files.length}] Processing ${file}...`)
+    try {
+      const fileData = JSON.parse(readFileSync(file, 'utf-8'))
+      const operations = fileData.results.flatMap((data: any) => {
+        if (!data.ticket_id) {
+          console.log(
+            `[Warning #${nextWarningId++}] Missing ticket_id`,
+            JSON.stringify(data),
+          )
+          return []
+        }
+        const info = EntryInfo.parse(data)
+        const updated = new Date(Date.parse(info.last_activity))
+        updated.setMilliseconds(0)
+        const operation: AnyBulkWriteOperation<TicketSnapshot> = {
+          updateOne: {
+            filter: { 'data.ticket_id': info.ticket_id, updated },
+            update: { $setOnInsert: { data, updated } },
+            upsert: true,
+          },
+        }
+        return [operation]
+      })
+      const writeResult = await collection.bulkWrite(operations)
+      const nProcessed = operations.length
+      const nAdded = writeResult.getUpsertedIds().length
+      totalProcessed += nProcessed
+      totalAdded += nAdded
+      console.log(
+        `Processed ${file} (${totalProcessed} (+${nProcessed}) processed, ${totalAdded} (+${nAdded}) added)`,
+      )
+    } catch (error) {
+      console.error(`Unable to process ${file}`, error)
     }
-  } finally {
-    await client.close()
   }
 }
 
-main()
+withDb(main)
