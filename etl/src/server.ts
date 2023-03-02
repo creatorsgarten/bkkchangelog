dee0b8860c3b1bdf7e62ea4df6a3d7a30e130d44
Thai Pangsakulyanont <dtinth@spacet.me>
2023-03-02 10:50:46 +0000

    add method to get by id

--- etl/src/server.ts
+++ etl/src/server.ts
@@ -1,4 +1,4 @@
-import { initTRPC } from '@trpc/server'
+import { initTRPC, TRPCError } from '@trpc/server'
 import Fastify, { FastifyInstance, FastifyRequest } from 'fastify'
 import {
   FastifyTRPCPluginOptions,
@@ -35,90 +35,124 @@ const parseDate = (value?: string) => {
   return date && !isNaN(date.getTime()) ? date : null
 }
 
-export const appRouter = t.router({
-  getChangelog: t.procedure
-    .input(
-      z.object({
-        sort: z.enum(['asc', 'desc']).default('desc'),
-        since: z.string().datetime().optional(),
-        until: z.string().datetime().optional(),
-      }),
-    )
-    .query(async ({ input }) => {
-      const client = await dbPromise
-      const snapshots = getTicketSnapshotsCollection(client)
-      const changelog = getChangelogCollection(client)
-      const since = parseDate(input.since)
-      const until = parseDate(input.until)
-      const filter =
-        since || until
-          ? {
-              finished: {
-                ...(since ? { $gte: since } : {}),
-                ...(until ? { $lte: until } : {}),
-              },
-            }
-          : {}
-      let cursor = changelog
-        .find(filter)
-        .sort({ finished: input.sort === 'asc' ? 1 : -1 })
-        .limit(64)
-      const totalPromise = changelog.countDocuments(filter)
-      const items = await cursor.toArray()
-      const foundSnapshots = new Map(
-        await snapshots
-          .find({ _id: { $in: items.map((item) => item.ticketSnapshotId) } })
-          .toArray()
-          .then((results) =>
-            results.map((result) => [result.data.ticket_id, result]),
-          ),
-      )
-      const results = items.flatMap(({ ticketSnapshotId, ...rest }) => {
-        const snapshot = foundSnapshots.get(rest._id)
-        if (!snapshot) return []
-        return [{ ...rest, snapshot }]
-      })
-      const total = await totalPromise
-      return { total, results }
+const getChangelog = t.procedure
+  .input(
+    z.object({
+      sort: z.enum(['asc', 'desc']).default('desc'),
+      since: z.string().datetime().optional(),
+      until: z.string().datetime().optional(),
     }),
-
-  countChangelogEntries: t.procedure
-    .input(
-      z.object({
-        since: z.string().datetime(),
-        until: z.string().datetime(),
-      }),
-    )
-    .query(async ({ input }) => {
-      const client = await dbPromise
-      const changelog = getChangelogCollection(client)
-      const totalBeforeUntilPromise = changelog.countDocuments({
-        finished: { $gte: new Date(input.since), $lt: new Date(input.until) },
-      })
-      const idsOnUntilPromise = changelog
-        .find({ finished: new Date(input.until) }, { projection: { _id: 1 } })
+  )
+  .query(async ({ input }) => {
+    const client = await dbPromise
+    const snapshots = getTicketSnapshotsCollection(client)
+    const changelog = getChangelogCollection(client)
+    const since = parseDate(input.since)
+    const until = parseDate(input.until)
+    const filter =
+      since || until
+        ? {
+            finished: {
+              ...(since ? { $gte: since } : {}),
+              ...(until ? { $lte: until } : {}),
+            },
+          }
+        : {}
+    let cursor = changelog
+      .find(filter)
+      .sort({ finished: input.sort === 'asc' ? 1 : -1 })
+      .limit(64)
+    const totalPromise = changelog.countDocuments(filter)
+    const items = await cursor.toArray()
+    const foundSnapshots = new Map(
+      await snapshots
+        .find({ _id: { $in: items.map((item) => item.ticketSnapshotId) } })
         .toArray()
-        .then((results) => results.map((result) => result._id))
-      return {
-        totalBeforeUntil: await totalBeforeUntilPromise,
-        idsOnUntil: await idsOnUntilPromise,
-      }
-    }),
+        .then((results) =>
+          results.map((result) => [result.data.ticket_id, result]),
+        ),
+    )
+    const results = items.flatMap(({ ticketSnapshotId, ...rest }) => {
+      const snapshot = foundSnapshots.get(rest._id)
+      if (!snapshot) return []
+      return [{ ...rest, snapshot }]
+    })
+    const total = await totalPromise
+    return { total, results }
+  })
 
-  getTicketSnapshots: t.procedure
-    .input(z.object({ id: z.string() }))
-    .query(async ({ input }) => {
-      const client = await dbPromise
-      const snapshots = getTicketSnapshotsCollection(client)
-      const filter = { 'data.ticket_id': input.id }
-      const countPromise = snapshots.countDocuments(filter)
-      const snapshotsCursor = snapshots
-        .find(filter)
-        .sort({ updated: -1 })
-        .limit(64)
-      const total = await countPromise
-      return { total, results: await snapshotsCursor.toArray() }
-    }),
+const getTicketSnapshots = t.procedure
+  .input(z.object({ id: z.string() }))
+  .query(async ({ input }) => {
+    const client = await dbPromise
+    const snapshots = getTicketSnapshotsCollection(client)
+    const filter = { 'data.ticket_id': input.id }
+    const countPromise = snapshots.countDocuments(filter)
+    const snapshotsCursor = snapshots
+      .find(filter)
+      .sort({ updated: -1 })
+      .limit(64)
+    const total = await countPromise
+    return { total, results: await snapshotsCursor.toArray() }
+  })
+
+export const appRouter = t.router({
+  getChangelog,
+  getTicketSnapshots,
+
+  changelogEntries: t.router({
+    list: getChangelog,
+    get: t.procedure
+      .input(z.object({ id: z.string() }))
+      .query(async ({ input }) => {
+        const client = await dbPromise
+        const changelog = getChangelogCollection(client)
+        const entry = await changelog.findOne({ _id: input.id })
+        if (!entry) {
+          throw new TRPCError({
+            code: 'NOT_FOUND',
+            message: `Changelog entry ${input.id} not found`,
+          })
+        }
+        const { ticketSnapshotId, ...rest } = entry
+        const snapshot = await getTicketSnapshotsCollection(client).findOne({
+          _id: ticketSnapshotId,
+        })
+        if (!snapshot) {
+          throw new TRPCError({
+            code: 'NOT_FOUND',
+            message: `Snapshot ${ticketSnapshotId} not found for changelog entry ${input.id}`,
+          })
+        }
+        return { ...rest, snapshot }
+      }),
+    count: t.procedure
+      .input(
+        z.object({
+          since: z.string().datetime(),
+          until: z.string().datetime(),
+        }),
+      )
+      .query(async ({ input }) => {
+        const client = await dbPromise
+        const changelog = getChangelogCollection(client)
+        const totalBeforeUntilPromise = changelog.countDocuments({
+          finished: { $gte: new Date(input.since), $lt: new Date(input.until) },
+        })
+        const idsOnUntilPromise = changelog
+          .find({ finished: new Date(input.until) }, { projection: { _id: 1 } })
+          .toArray()
+          .then((results) => results.map((result) => result._id))
+        return {
+          totalBeforeUntil: await totalBeforeUntilPromise,
+          idsOnUntil: await idsOnUntilPromise,
+        }
+      }),
+  }),
+
+  ticketSnapshots: t.router({
+    list: getTicketSnapshots,
+  }),
 })
 
 export type AppRouter = typeof appRouter
