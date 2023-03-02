fba57fb2e85b1cde37928f2cb44b719d0194b029
Thai Pangsakulyanont <dtinth@spacet.me>
2023-03-02 12:47:04 +0000

    make image generation script easier to use

--- poster/src/generate.ts
+++ poster/src/generate.ts
@@ -2,6 +2,8 @@ import 'fetch-types'
 import { mkdirSync, readFileSync, writeFileSync } from 'fs'
 import { parseArgs } from 'util'
 import { generateImage } from './_image'
+import { client } from './_api'
+import { getTweet } from './_tweet'
 
 async function main() {
   const { positionals } = parseArgs({ allowPositionals: true })
@@ -10,13 +12,15 @@ async function main() {
       'Expected exactly one positional argument for the ticket ID',
     )
   }
-  const ticketId = positionals[0]
-  const ticketPath = `.data/tickets/${ticketId}.json`
-  const snapshots = JSON.parse(readFileSync(ticketPath, 'utf-8'))
+
+  const id = positionals[0]
+  const entry = await client.changelogEntries.get.query({ id })
+  const { results: snapshots } = await client.ticketSnapshots.list.query({ id })
   snapshots.sort((a: any, b: any) => (a.updated < b.updated ? -1 : 1))
-  const latest = snapshots[snapshots.length - 1]
-  const image = await generateImage(latest)
-  const imagePath = `.data/output/${ticketId}.jpg`
+  const tweet = getTweet(entry, snapshots)
+  console.log(tweet)
+  const image = await generateImage(entry.snapshot)
+  const imagePath = `.data/output/${id}.jpg`
   mkdirSync('.data/output', { recursive: true })
   writeFileSync(imagePath, image)
   console.log(imagePath)
